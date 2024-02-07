0b3fba3e1df051b13134bd1ac5a212d2590b7023
Farlaf <slepuhovoleg@gmail.com>
2024-02-07 12:55:39 +0700

    fixed product new and destroy bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :stocks
+  has_many :stocks, dependent: :destroy
 
   has_many_attached :images do |img|
     img.variant :thumb, resize_to_limit: [50, 50]
