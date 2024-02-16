a46f8e21af894928f6e5e95c39f6de88f0eb9eef
Farlaf <slepuhovoleg@gmail.com>
2024-02-16 17:37:51 +0700

    added orderpoduct model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :stocks, dependent: :destroy
+  has_many :order_products
 
   has_many_attached :images do |img|
     img.variant :thumb, resize_to_limit: [50, 50]
