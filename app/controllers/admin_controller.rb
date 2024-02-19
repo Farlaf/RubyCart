5cd703dd305635b7d9ab9686f4f88ba5b22b307d
Farlaf <slepuhovoleg@gmail.com>
2024-02-19 11:35:07 +0700

    added quick stats

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,5 +2,13 @@ class AdminController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
 
-  def index; end
+  def index
+    @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+    @quick_stats = {
+      sales: Order.where(created_at: Time.now.getlocal.midnight..Time.now.getlocal).count,
+      revenue: Order.where(created_at: Time.now.getlocal.midnight..Time.now.getlocal).sum(:total).round,
+      avg_sale: Order.where(created_at: Time.now.getlocal.midnight..Time.now.getlocal).average(:total).round,
+      per_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.getlocal.midnight..Time.now.getlocal }).average(:quantity)
+    }
+  end
 end
