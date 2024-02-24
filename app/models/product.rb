96be5bacb25f929b3a6a8429f3dd389121d6bdaa
Farlaf <slepuhovoleg@gmail.com>
2024-02-24 17:11:46 +0700

    add category page

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,6 @@ class Product < ApplicationRecord
 
   has_many_attached :images do |img|
     img.variant :thumb, resize_to_limit: [50, 50]
+    img.variant :medium, resize_to_limit: [250, 250]
   end
 end
