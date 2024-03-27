0b2d2f6753a2fa932308aa76673e51aff5ef4e02
Farlaf <slepuhovoleg@gmail.com>
2024-03-27 21:49:18 +0700

    added cart items and clear btn

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,67 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="cart"
+export default class extends Controller {
+  initialize() {
+    const cart = JSON.parse(localStorage.getItem("cart"))
+
+    if (!cart || cart.length <= 0) {
+      return
+    }
+
+    let total = 0
+    let cartItems = document.getElementById("cart_items")
+
+    for(let i = 0; i < cart.length; i++ ) {
+      total += cart[i].price * cart[i].quantity
+
+      let removeBtn = document.createElement("button")
+      removeBtn.value = i
+      removeBtn.addEventListener("click", this.removeFromCart)
+      removeBtn.classList.add("font-medium", "text-indigo-600", "hover:text-indigo-500")
+      removeBtn.innerText = "Remove"
+
+      let cartItem = document.createElement("li")
+      cartItem.classList.add("flex", "py-6")
+      cartItem.innerHTML = `<div class="flex flex-1 flex-col">
+          <div>
+            <div class="flex justify-between text-base font-medium text-gray-900">
+              <h3>
+                ${cart[i].name}
+              </h3>
+              <p class="ml-4">$${cart[i].price}</p>
+            </div>
+            <p class="mt-1 text-sm text-gray-500">Size: ${cart[i].size}</p>
+          </div>
+          <div class="flex flex-1 items-end justify-between text-sm">
+            <p class="text-gray-500">Qty ${cart[i].quantity}</p>
+            <div class="flex remove-btn-container"> </div>
+          </div>
+        </div>`
+
+      cartItem.querySelector('.remove-btn-container').appendChild(removeBtn);
+      cartItems.appendChild(cartItem)
+    }
+
+    let cartTotal = document.getElementById("cart-total")
+    cartTotal.innerHTML = `<div class="border-t border-gray-200 mt-6 py-6">
+        <div class="flex justify-between text-base font-medium text-gray-900">
+          <p>Subtotal</p>
+          <p>$${total}</p>
+        </div>
+        <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+      </div>`
+  }
+  removeFromCart(event) {
+    let cart = JSON.parse(localStorage.getItem("cart"))
+    const id = event.target.value
+    const index = cart.findIndex(item => item.id === id)
+    cart.splice(index, 1)
+    localStorage.setItem("cart", JSON.stringify(cart))
+    window.location.reload()
+  }
+  clearAll() {
+    localStorage.removeItem("cart")
+    window.location.reload()
+  }
+}
